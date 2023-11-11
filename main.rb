183c9681a5d016d52a3584ec92c7f10e526d8f96
Turpin Raphaël <raphael.turpin@epitech.eu>
2023-11-11 13:10:19 +0400

    Sandbox

--- main.rb
+++ main.rb
@@ -1,14 +1,65 @@
 require 'ruby2d'
 
-# Define a square shape.
-@square = Square.new(x: 10, y: 10, size: 50, color: 'blue')
-@wall = Square.new(x: 0, y: 100, size: 2, color: 'red')
+set width: 960
+set height: 540
 
 # Define the initial speed (and direction).
 @x_speed = 0
 @y_speed = 0
 
-@speed = 2
+@speed = 2.6
+
+tileset = Tileset.new(
+    './assets/sandbox.png',
+    tile_width: 16,
+    tile_height: 16,
+    padding: 0,
+    spacing: 0,
+    scale: 2
+)
+
+# TILESET DE 80x45
+
+tileset.define_tile('white', 0, 0)
+tileset.define_tile('black', 0, 1)
+tileset.define_tile('white_wall_top', 1, 0)
+tileset.define_tile('black_wall_top', 1, 1)
+tileset.define_tile('white_wall_left', 1, 0, rotate: -90)
+tileset.define_tile('black_wall_left', 1, 1, rotate: -90)
+tileset.define_tile('white_wall_down', 1, 0, rotate: 180)
+tileset.define_tile('black_wall_down', 1, 1, rotate: 180)
+tileset.define_tile('white_wall_right', 1, 0, rotate: 90)
+tileset.define_tile('black_wall_right', 1, 1, rotate: 90)
+tileset.define_tile('white_corner_topleft', 2, 0)
+tileset.define_tile('black_corner_topleft', 2, 1)
+tileset.define_tile('white_corner_topright', 2, 0, rotate: 90)
+tileset.define_tile('black_corner_topright', 2, 1, rotate: 90)
+tileset.define_tile('white_corner_bottomright', 2, 0, rotate: 180)
+tileset.define_tile('black_corner_bottomright', 2, 1, rotate: 180)
+tileset.define_tile('white_corner_bottomleft', 2, 0, rotate: -90)
+tileset.define_tile('black_corner_bottomleft', 2, 1, rotate: -90)
+
+tileset.set_tile('white_corner_topleft', [
+    {x: 0, y: 0}
+])
+
+tileset.set_tile('black_wall_top', [
+    {x: 32, y: 0}
+])
+
+tileset.set_tile('black_wall_left', [
+    {x: 0, y: 32}
+])
+
+tileset.set_tile('white', [
+    {x: 32, y: 32}
+])
+
+@square = Square.new(x: 10, y: 10, size: 32, color: 'blue')
+@walls = [
+    Rectangle.new(x: 0, y: 96, width: 64, height: 32, color: 'red'),
+    Rectangle.new(x: 32, y: 144, width: 64, height: 64, color: 'red'),
+]
 
 # Define what happens when a specific key is pressed.
 # Each keypress influences on the  movement along the x and y axis.
@@ -42,50 +93,53 @@ update do
         @y_speed = 0
     end
 
-    checkY = @square.y + @square.height > @wall.y && @square.y < @wall.y + @wall.height
-    checkX = @square.x + @square.width > @wall.x && @square.x < @wall.x + @wall.width
+    @walls.each do |wall|
 
-    above = @square.y + @square.height <= @wall.y
-    below = @square.y >= @wall.y + @wall.height
-    left = @square.x + @square.width <= @wall.x
-    right = @square.x >= @wall.x + @wall.width
+        checkY = @square.y + @square.height > wall.y && @square.y < wall.y + wall.height
+        checkX = @square.x + @square.width > wall.x && @square.x < wall.x + wall.width
 
-    if checkX || checkY
-        if above
-            if @square.y + @square.height + @y_speed > @wall.y
-                @square.color = 'purple'
-                @square.y = @wall.y - @square.height
-                @y_speed = 0
-            else
-                @square.color = 'yellow'
+        above = @square.y + @square.height <= wall.y
+        below = @square.y >= wall.y + wall.height
+        left = @square.x + @square.width <= wall.x
+        right = @square.x >= wall.x + wall.width
+
+        if checkX || checkY
+            if above
+                if @square.y + @square.height + @y_speed > wall.y
+                    @square.color = 'purple'
+                    @square.y = wall.y - @square.height
+                    @y_speed = 0
+                else
+                    @square.color = 'yellow'
+                end
+            elsif below
+                if @square.y + @y_speed < wall.y + wall.height
+                    @square.color = 'purple'
+                    @square.y = wall.y + wall.height
+                    @y_speed = 0
+                else
+                    @square.color = 'green'
+                end
+            elsif left
+                if @square.x + @square.width + @x_speed > wall.x
+                    @square.color = 'purple'
+                    @square.x = wall.x - @square.width
+                    @x_speed = 0
+                else
+                    @square.color = 'red'
+                end
+            elsif right
+                if @square.x + @x_speed < wall.x + wall.width
+                    @square.color = 'purple'
+                    @square.x = wall.x + wall.width
+                    @x_speed = 0
+                else
+                    @square.color = 'orange'
+                end
             end
-        elsif below
-            if @square.y + @y_speed < @wall.y + @wall.height
-                @square.color = 'purple'
-                @square.y = @wall.y + @wall.height
-                @y_speed = 0
-            else
-                @square.color = 'green'
-            end
-        elsif left
-            if @square.x + @square.width + @x_speed > @wall.x
-                @square.color = 'purple'
-                @square.x = @wall.x - @square.width
-                @x_speed = 0
-            else
-                @square.color = 'red'
-            end
-        elsif right
-            if @square.x + @x_speed < @wall.x + @wall.width
-                @square.color = 'purple'
-                @square.x = @wall.x + @wall.width
-                @x_speed = 0
-            else
-                @square.color = 'orange'
-            end
+        else
+            @square.color = 'blue'
         end
-    else
-        @square.color = 'blue'
     end
 
     @square.x += @x_speed
